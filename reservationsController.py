b3097f1a801454d7c2f919c7233c169541940f8e
Michael W Warren, Sr <47991799+mwarrens63@users.noreply.github.com>
2023-02-25 22:38:13 -0500

    Add files via upload

---
+++ reservationsController.py
@@ -0,0 +1,92 @@
+# Connects reservationsView with reservationsModel.
+# Provides high-level CRUD methods connecting reservationsView to reservationsModel.
+# Converts reservationsModel data to a format usable by reservationsView.
+# Updates reservationsView when the reservationsModel has changed.
+
+from PySimpleGUI import TreeData
+import reservationsModel as rm
+
+
+# Converts dict returned by getReservationsData() to
+# list of lists: each list is a row in the Reservations table	
+def _retrieveReservationsList() -> list:
+	tableData = rm.getReservationsData()
+	valuesList = [*[list(idx.values()) for idx in tableData ]]
+	return(valuesList)
+
+
+# Converts dict returned by getDestinationsData() to
+# list of lists: each list is a row in the Destinations table
+def _retrieveDestinationsList() -> list:
+	tableData = rm.getDestinationsData()
+	valuesList = [*[list(idx.values()) for idx in tableData ]]
+	return(valuesList)
+
+    
+def retrieveTreeColumnNames() -> list:
+    columnNames = rm.retrieveColumnNames()
+    return(columnNames)
+
+
+# PySimpleGUI.TreeData
+def _retrieveReservationsTreeData() -> TreeData:
+    treedata = TreeData()
+    
+    reservationsList = _retrieveReservationsList()
+
+    # Convert to PySimpleGUI TreeData format
+    n = 0
+    for res in reservationsList:
+        n += 1
+        keystr = "-row_{}-".format(n)
+        treedata.Insert('', key=keystr, text= n, values=res)
+        
+    return(treedata)
+
+
+def retrieveReservations() -> TreeData:
+    treedata = _retrieveReservationsTreeData()
+    return(treedata)
+            
+            
+def retrieveDestinations() -> list:
+    destinations = _retrieveDestinationsList()
+    return(destinations)
+
+
+def deleteReservation(idval, viewRef=None) -> None:
+    rm.deleteReservation(idval)
+    # Update Reservations Window Tree
+    if viewRef != None:
+        viewRef.treedata = _retrieveReservationsTreeData()
+        viewRef.refreshTree()
+    return()
+
+
+def createReservation(name, gender, passport_num, destination, departure_dt, arrival_dt) -> None:
+    rm.createReservation(name, gender, passport_num, destination, departure_dt, arrival_dt)
+    return()
+
+
+def updateReservation(idVal, name, gender, passport_num, destination, departure_dt, arrival_dt, viewRef=None) -> None:
+    rm.updateReservation(idVal, name, gender, passport_num, destination, departure_dt, arrival_dt)
+    # Update Reservations Window Tree
+    if viewRef != None:
+        viewRef.treedata = _retrieveReservationsTreeData()
+        viewRef.refreshTree()
+    return()
+
+
+# For tesing   
+def main():
+    
+    lst = retrieveTreeColumnNames()
+    print("Column Names: ", lst)
+    print()
+    TreeData = _retrieveReservationsTreeData()
+    print(TreeData)
+    # createReservation('Robert Warren', 'Male', 'M897645', 'Tokyo', '2022-10-20 21:07:54', '2022-10-21 21:07:57')
+    
+
+if __name__ == '__main__':
+	main()
